18a65587755156299e0f26558e7252f30592df90
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-10-09 23:49:39 +0300

    Примеры useEffect

---
+++ src/examples/effect.js
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from "react";
+
+
+const App = () => {
+  const [type, setType] = useState(`users`);
+  const [data, setData] = useState([]);
+  const [pos, setPos] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  /*
+  console.log(`Component render`);
+
+  useEffect(() => {
+    console.log(`render`);
+  });
+  */
+  /*
+  useEffect(() => {
+    console.log(`Type change`, type);
+  }, [type]);
+  */
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+
+    return () => {
+      console.log(`clean type`);
+    };
+  }, [type]);
+
+  const mouseMoveHandler = (event) => {
+    setPos({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  }
+
+  useEffect(() => {
+    console.log(`ComponentDidMount`);
+
+    window.addEventListener(`mousemove`, mouseMoveHandler);
+
+    return () => {
+      window.removeEventListener(`mousemove`, mouseMoveHandler);
+    };
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <h1>Ресурс: {type}</h1>
+
+      <div>
+        <button onClick={() => setType(`users`)}>
+          Пользователи
+        </button>
+        <button onClick={() => setType(`todos`)}>
+          Todos
+        </button>
+        <button onClick={() => setType(`posts`)}>
+          Посты
+        </button>
+      </div>
+
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+
+      <pre>{JSON.stringify(pos, null, 2)}</pre>
+    </div>
+  );
+};
+
+
+export default App;
